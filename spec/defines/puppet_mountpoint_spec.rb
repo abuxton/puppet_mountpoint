c525ad2230e37cec23750f84a7e3f204a324d20a
Adam Buxton <buxton.adam@gmail.com>
2018-10-12 23:18:56 +0100

    validat rspec!

--- spec/defines/puppet_mountpoint_spec.rb
+++ spec/defines/puppet_mountpoint_spec.rb
@@ -1,24 +1,23 @@
 require 'spec_helper'
-#describe 'puppet_mountpoint' do
 describe 'puppet_mountpoint', type: :define do
-  let(:title) { 'namevar' }
-  let(:params) do
-    { 'mountpoint'        => 'namevar',
-      'legacy_auth'       => 'false',
-      'path'              => '/tmp/namevar',
-      'auth_allow_ip'     =>  nil,
-      'auth_allow'        =>  '*',
-      'hocon_allow'       =>  '*',
-      'fileserverconfig'  => 'etc/puppetlabs/puppet/fileserver.conf',
-      'legacy_rest_auth'  => '/etc/puppetlabs/puppet/auth.conf',
-      'hocon_auth'        => '/etc/puppetlabs/puppetserver/conf.d/auth.conf',
-      'hocon_hash'        => {}
-    } 
-  end
-
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:title) { 'test' }
+      let(:params) do
+        { 'mountpoint'        => 'test',
+          'legacy_auth'       => 'false',
+          'path'              => '/tmp/namevar',
+          'auth_allow_ip'     =>  nil,
+          'auth_allow'        =>  '*',
+          'hocon_allow'       =>  '*',
+          'fileserverconfig'  => 'etc/puppetlabs/puppet/fileserver.conf',
+          'legacy_rest_auth'  => '/etc/puppetlabs/puppet/auth.conf',
+          'hocon_auth'        => '/etc/puppetlabs/puppetserver/conf.d/auth.conf',
+          'hocon_hash'        => {},
+          'settings::fileserverconfig' => '',
+          'settings::rest_authconfig' => '' }
+      end
 
       it { is_expected.to compile }
     end
