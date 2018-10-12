b34ab9d95ab29ed16f42427476daad7ba27bd3c0
Adam Buxton <buxton.adam@gmail.com>
2018-10-12 19:58:54 +0100

    develop define type for mountpoint

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,5 +5,73 @@
 # @example
 #   puppet_mountpoint { 'namevar': }
 define puppet_mountpoint(
+  String                        $mountpoint     => $title,
+  Boolean                       $legacy_auth    => false,
+  Stdlib::Absolutepath          $path           => '',
+  Optional                      $auth_allow     =>  '*',
+  Optional[Stdlib::IP::Address] $auth_allow_ip  =>  undef,
+  Optional                      $hocon_allow    =>  '*',
 ) {
+  $fileserverconfig = $settings::fileserverconfig #/etc/puppetlabs/puppet/fileserver.conf
+  $legacy_rest_auth        = $settings::rest_authconfig # /etc/puppetlabs/puppet/auth.conf
+  $hocon_auth = '/etc/puppetlabs/puppetserver/conf.d/auth.conf'
+  if $legacy_auth {
+    file_line { "Legacy Auth Path setting comment for ${mountpoint}":
+      ensure              => present,
+      path                => $legacy_rest_auth,
+      line                => "# Allow limited access to files in $path",
+      append_on_no_match  => true,
+    }
+    file_line { "Legacy Auth Path setting for ${mountpoint}":
+      ensure  => present,
+      path    => $legacy_rest_auth,
+      line  => "path ~ ^/file_(metadata|content)s?/${mountpoint}/",
+      after   => "# Allow limited access to files in $path",
+      require => file_line["Legacy Auth Path setting comment for ${mountpoint}"],
+    }
+    file_line { "Legacy Auth setting ${mountpoint}":
+      ensure  => present,
+      path    => $legacy_rest_auth,
+      line    => "auth = yes #${mountpoint}",
+      after   => "path ~ ^/file_(metadata|content)s?/${mountpoint}/",
+      require => file_line["Legacy Auth Path setting for ${mountpoint}"],
+    }
+    if $auth_allow {
+      file_line { "Legacy Allow setting ${mountpoint}":
+        ensure  => present,
+        path    => $legacy_rest_auth,
+        line    => "allow ${auth_allow} #${mountpoint}",
+        after   => "auth = yes #${mountpoint}",
+        require => file_line["auth = yes #${mountpoint}"],
+      }
+    }
+    if $auth_allow_ip{
+      file_line { "Legacy Allow_IP setting ${mountpoint}":
+        ensure  => present,
+        path    => $legacy_rest_auth,
+        line    => "allow ${auth_allow} #${mountpoint}",
+        after   => "auth = yes #${mountpoint}",
+        require => file_line["auth = yes #${mountpoint}"],
+      }
+    }
+  }
+  else {
+
+    hocon_setting { 'Auth setting for ${mountpoint}':
+      ensure => present,
+      path => $hocon_auth,
+      setting => 'hash_setting',
+      value => { 'match-request' => {
+                    'path' => "^/puppet/v3/file_(content|metadata)s?/${mountpoint}",
+                    'type' => 'regex'
+                    },
+                  'allow'       => $hocon_allow,
+                  'sort-order'  => '400',
+                  'name'        => "Auth setting for ${mountpoint}"
+                },
+    }
+
+  }
+
+
 }
